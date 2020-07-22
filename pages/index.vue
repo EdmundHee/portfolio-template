823749fc306ec16cb2027a93adceddbb4aa5743f
Edmund Hee <edmund.hee05@gmail.com>
2020-07-22 21:16:14 +0800

    Updated with my content

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,108 @@
 <template>
   <div class="flex flex-col w-screen h-full">
     <!-- Your name, profile picture and contact -->
-    <HeadWrapper 
-      name='Your Name'
-      title='Your Job Title'
-      userImgURL='Image URL Address'
-      phone='Your Contact Number'
-      email='Your Email Contact'
+    <HeadWrapper
+      name="Edmund Hee"
+      title="Full Stack Developer"
+      userImgURL="../img/Edmund.jpeg"
+      phone="+0123456789"
+      email="edmund@thelead.io"
     />
     <!-- About Me -->
-    <AboutWrapper 
-      aboutMe='Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.'
-      linkedInURL='Your LinkedIn URL Address'
-      languages='Languages'
-      nationality='Your Nationality'
-      interest='Your Interest'
+    <AboutWrapper
+      aboutMe="I'm a full stack web developer and self-taught Data Scientist"
+      linkedInURL="https://www.linkedin.com/in/edmundhee/"
+      languages="English, Chinese & Malay"
+      nationality="Malaysian"
+      interest="Coffee"
     />
     <!-- Working Experience -->
     <CareerWrapper>
       <template v-slot:career>
-        <Career 
-          companyName='Company Name'
-          title='Your Job Title'
-          startDate='Start Date'
-          endDate='End Date'
-          >
+        <Career
+          companyName="AirAsia"
+          title="Data Scientist"
+          startDate="Nov 2017"
+          endDate="Nov 2019"
+        >
           <!-- List of what you had did in the company -->
           <template v-slot:experience>
-            <ul class='text-gray-600 list-inside list-disc mt-2 px-4'>
+            <ul class="text-gray-600 list-inside list-disc mt-2 px-4">
               <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
               <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
               <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
             </ul>
           </template>
         </Career>
-        
-        <Career 
-          companyName='Company Name'
-          title='Your Job Title'
-          startDate='Start Date'
-          endDate='End Date'
-          >
+
+        <Career
+          companyName="SPNQ Software Hub"
+          title="Full Stack Web Developer"
+          startDate="July 2016"
+          endDate="Nov 2017"
+        >
           <!-- List of what you had did in the company -->
           <template v-slot:experience>
-            <ul class='text-gray-600 list-inside list-disc mt-2 px-4'>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
-              <li>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, nihil. Aut sapiente ipsa fugiat quasi nisi officiis cumque, amet enim corporis. Pariatur distinctio maxime sint, ducimus quos quia nostrum atque.</li>
+            <ul class="text-gray-600 list-inside list-disc mt-2 px-4">
+              <li>Built Business Web Application for business users</li>
+              <li>Created Data Warehouse solutions for the company</li>
+              <li>Created single source of true for the client</li>
+              <li>Created single source of true for the client</li>
+              <li>Created single source of true for the client</li>
             </ul>
           </template>
         </Career>
       </template>
     </CareerWrapper>
     <!-- Achievement -->
-    <AchievementWrapper 
-      project='Number of Project'
-      client='Number of Happy Clients'
-      award='Number of Awards'
-    />
+    <AchievementWrapper project="15" client="15k" award="10"/>
     <!-- Portfolio -->
     <PortfolioWrapper>
-        <template v-slot:portfolio>
-          <div class='grid grid-cols-1 md:grid-cols-3'>
-            <ProjectCard 
-              imgUrl='../img/github.png'
-              name='Project Name'
-              project='Project Type'
-              projectURL='https://github.com'
-              />
+      <template v-slot:portfolio>
+        <div class="grid grid-cols-1 md:grid-cols-3">
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="My own Portfolio"
+            project="NuxtJS Project"
+            projectURL="https://github.com/edmundhee/portfolio-template"
+          />
 
-              <ProjectCard 
-              imgUrl='../img/github.png'
-              name='Project Name'
-              project='Project Type'
-              projectURL='https://github.com'
-              />
-              
-              <ProjectCard 
-              imgUrl='../img/github.png'
-              name='Project Name'
-              project='Project Type'
-              projectURL='https://github.com'
-              />
-          </div>
-        </template>
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="Project Name"
+            project="Project Type"
+            projectURL="https://github.com"
+          />
+
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="Project Name"
+            project="Project Type"
+            projectURL="https://github.com"
+          />
+
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="Project Name"
+            project="Project Type"
+            projectURL="https://github.com"
+          />
+
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="Project Name"
+            project="Project Type"
+            projectURL="https://github.com"
+          />
+
+          <ProjectCard
+            imgUrl="../img/github.png"
+            name="Project Name"
+            project="Project Type"
+            projectURL="https://github.com"
+          />
+        </div>
+      </template>
     </PortfolioWrapper>
   </div>
 </template>
@@ -92,8 +111,8 @@
 export default {
   head() {
     return {
-      title: 'Your Name'
-    }
+      title: "Edmund Hee | FullStack Developer"
+    };
   }
-}
+};
 </script>
